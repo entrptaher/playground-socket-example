d285e4faeef37c3c7827070d5739978c8c7744ea
Glitch (slime-activity) <none>
2018-05-02 12:44:07 +0000

    🚉🤒 Updated with Glitch

--- server.js
+++ server.js
@@ -2,19 +2,31 @@ const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const Datastore = require("nedb");
+
+// by default will run on port 3000
 const port = process.env.PORT || 3000;
 server.listen(port);
 
+// create a sample nedb database and automatically load it for next usage
 const db = new Datastore({ filename: "cars", autoload: true });
 
+/**
+ * Get last x locations for specific car
+ * @param {String} id 
+ * @param {Number} limit
+ */
 function getLast({ id, limit = 0 }) {
   return new Promise((resolve, reject) => {
+    
+    // find specific car
     db.findOne({ _id: id }, (err, doc) => {
       if (err) {
         return reject(err);
       }
       try {
+        // extract the location
         const { location } = doc;
+        // cut of specific part from location array
         const response = location.slice(
           location.length - limit,
           location.length
@@ -27,45 +39,54 @@ function getLast({ id, limit = 0 }) {
   });
 }
 
+// simple html pages
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/views/index.html`);
+    res.sendFile(`${__dirname}/index.html`);
 });
 
 app.get("/user", (req, res) => {
-  res.sendFile(`${__dirname}/views/user.html`);
+  res.sendFile(`${__dirname}/user.html`);
 });
 
 app.get("/listener", (req, res) => {
-  res.sendFile(`${__dirname}/views/listener.html`);
+  res.sendFile(`${__dirname}/listener.html`);
 });
 
+// socket controller
 io.on("connection", socket => {
   console.log("user connected");
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 
+  // if user wants to join a room, add them and send success
   socket.on("join", (data, fn) => {
     console.log(data, fn);
     socket.join(data.room);
     fn({ status: "ok", room: data.room });
   });
 
+  // user sends location data
   socket.on("clientData", data => {
+    // save the location data on database
     db.update(
       { _id: data.id },
       { $push: { location: data.location } },
-      { upsert: true },
+      { upsert: true }, // if collection is not available then create it
       (err, numReplaced, upsert) => {
         // console.log(err, numReplaced, upsert);
       }
     );
+    // emit the data to all users/listeners except the sender
     socket.to(data.room).emit("getClientData", data);
   });
 
+  // listen for speicific car data
   socket.on("getSingleData", (data, fn) => {
     const id = data.id;
     const limit = data.limit;
+    
+    // get the data and send in callback
     getLast({ id, limit })
       .then(locationList => {
         fn({ room: "cars", id: data.id, locationList });
